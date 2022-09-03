e52e072d9a3faacef48a79a3bf105c30f8a21950
Roman Lysenko <romashkaxxxl@gmail.com>
2022-09-03 10:55:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,36 +2,31 @@
 // При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
 // При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
 // Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку
-
 import * as throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
 const SAVED_KEY = 'feedback-form-state';
-const formValue = {};
+const formValue = JSON.parse(localStorage.getItem(SAVED_KEY)) || {};
 
 form.addEventListener('input', throttle(onGetData, 500));
 form.addEventListener('submit', sendForm);
 
 function onGetData(event) {
-  formValue[event.target.name] = event.target.value;
-  const { email, message } = formValue;
-  localStorage.setItem(SAVED_KEY, JSON.stringify({ email, message }));
+  const { name, value } = event.target;
+  formValue[name] = value; /* запитати у Руслана */
+  localStorage.setItem(SAVED_KEY, JSON.stringify(formValue));
 }
 
 function saveMessage() {
-  const obj = JSON.parse(localStorage.getItem(SAVED_KEY));
-  const textarea = document.querySelector('textarea');
-  const input = document.querySelector('input');
-  if (localStorage.getItem(SAVED_KEY)) {
-    textarea.value = obj.message;
-    input.value = obj.email;
-  }
+  const { email, message } = form.elements;
+  email.value = formValue.email || '';
+  message.value = formValue.message || '';
 }
 saveMessage();
 
 function sendForm(event) {
-  console.log(formValue);
   event.preventDefault();
-  event.target.reset();
+  console.log(formValue);
+  event.target.reset(); /* запитати у Руслана про currentTarget/target */
   localStorage.removeItem(SAVED_KEY);
 }
